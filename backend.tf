20892f91777fd83b144d15bba4a872e92d7e13ff
Rumesa <fatimarumesa2@gmail.com>
2026-04-04 02:00:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Rumesa"
-    storage_account_name = "rumesa"
-    container_name       = "tfstate"
+    resource_group_name  = "your_rg_name"
+    storage_account_name = "your_storage_account_name"
+    container_name       = "your_container"
     key                  = "terraform.tfstate"
   }
 }
